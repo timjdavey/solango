d8f4b005476b1499baaeb863ed04c96e854bd80a
Matthew Pontefract <mp@angelina.(none)>
2011-02-16 14:17:25 +0000

    trap exception thrown when opening non-existant logging config file. It's ugly and needs dealing with better.

--- solango/log.py
+++ solango/log.py
@@ -18,7 +18,12 @@ class LogManager:
         Instantiate the default logger.
         """
         if not logging.getLogger().handlers:
-            logging.config.fileConfig(LOGGING_CONF)
+            try:
+                logging.config.fileConfig(LOGGING_CONF)
+            except Exception, ex:
+                # @todo this is a VERY LAZY hack by MP
+                # to avoid crashing with no logging setup
+                pass
         self.logger = logging.getLogger('solango')
     
 log_manager = LogManager()
